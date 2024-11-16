4f88bffaafad254006363525cdf6aa4f2b61f092
Jayvercpsu <jayver.cpsu@gmail.com>
2024-11-16 22:07:25 +0800

    Adding new files view all professors

--- admin/includes/leftsidebar.php
+++ admin/includes/leftsidebar.php
@@ -1,76 +1,73 @@
-          
-            <div class="left side-menu">
-                <div class="sidebar-inner slimscrollleft">
+          <div class="left side-menu">
+              <div class="sidebar-inner slimscrollleft">
 
-                    <!--- Sidemenu -->
-                    <div id="sidebar-menu">
-                        <ul>
-                            <li class="menu-title">Navigation</li>
+                  <!--- Sidemenu -->
+                  <div id="sidebar-menu">
+                      <ul>
+                          <li class="menu-title">Navigation</li>
 
-                            <li class="has_sub">
-                                <a href="dashboard.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Dashboard </span> </a>
+                          <li class="has_sub">
+                              <a href="dashboard.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Dashboard </span> </a>
 
-                            </li>
+                          </li>
 
-                          
 
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Category </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="add-category.php">Add Category</a></li>
-                                    <li><a href="manage-categories.php">Manage Category</a></li>
-                                </ul>
-                            </li>
 
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="ti ti-layout-list-thumb"></i> <span>Sub Category </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="add-subcategory.php">Add Sub Category</a></li>
-                                    <li><a href="manage-subcategories.php">Manage Sub Category</a></li>
-                                </ul>
-                            </li>
-                          
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-newspaper"></i> <span> Posts (News) </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="add-post.php">Add Posts</a></li>
-                                    <li><a href="manage-posts.php">Manage Posts</a></li>
-                                    <li><a href="trash-posts.php">Trash Posts</a></li>
-                                </ul>
-                            </li>
-                          
+                          <li class="has_sub">
+                              <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Category </span> <span class="menu-arrow"></span></a>
+                              <ul class="list-unstyled">
+                                  <li><a href="add-category.php">Add Category</a></li>
+                                  <li><a href="manage-categories.php">Manage Category</a></li>
+                              </ul>
+                          </li>
 
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="ti ti-files"></i> <span> CMS </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="aboutus.php">About us</a></li>
-                                    <li><a href="contactus.php">Contact us</a></li>
-                                </ul>
-                            </li>
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-comment-account-outline"></i> <span> Comments </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="unapprove-comment.php">Waiting for Approval </a></li>
-                                    <li><a href="manage-comments.php">Approved Comments</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#" class="waves-effect"><i class="ti ti-info-alt"></i> <span>Watch Video</span></a>
+                          <li class="has_sub">
+                              <a href="javascript:void(0);" class="waves-effect"><i class="ti ti-layout-list-thumb"></i> <span>Sub Category </span> <span class="menu-arrow"></span></a>
+                              <ul class="list-unstyled">
+                                  <li><a href="add-subcategory.php">Add Sub Category</a></li>
+                                  <li><a href="manage-subcategories.php">Manage Sub Category</a></li>
+                              </ul>
+                          </li>
 
-                            </li>
-                            <li>
-                                <a href="#" class="waves-effect"><i class="fa fa-eye" aria-hidden="true"></i> <span>Website Preview</span></a>
+                          <li class="has_sub">
+                              <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-newspaper"></i> <span> Posts (News) </span> <span class="menu-arrow"></span></a>
+                              <ul class="list-unstyled">
+                                  <li><a href="add-post.php">Add Posts</a></li>
+                                  <li><a href="manage-posts.php">Manage Posts</a></li>
+                                  <li><a href="trash-posts.php">Trash Posts</a></li>
+                              </ul>
+                          </li>
+                          <li>
+                              <a href="all-student.php" class="waves-effect"><i class="fa fa-users" aria-hidden="true"></i> <span>All Students</span></a>
+                          </li>
+                          <li>
+                              <a href="all-professor.php" class="waves-effect"><i class="fa fa-users" aria-hidden="true"></i> <span>All Professors</span></a>
+                          </li>
 
-                            </li>
-                          
-                        </ul>
-                    </div>
-                    <!-- Sidebar -->
-                    <div class="clearfix"></div>
+                          <li class="has_sub">
+                              <a href="javascript:void(0);" class="waves-effect"><i class="ti ti-files"></i> <span> CMS </span> <span class="menu-arrow"></span></a>
+                              <ul class="list-unstyled">
+                                  <li><a href="aboutus.php">About us</a></li>
+                                  <li><a href="contactus.php">Contact us</a></li>
+                              </ul>
+                          </li>
+                          <li class="has_sub">
+                              <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-comment-account-outline"></i> <span> Comments </span> <span class="menu-arrow"></span></a>
+                              <ul class="list-unstyled">
+                                  <li><a href="unapprove-comment.php">Waiting for Approval </a></li>
+                                  <li><a href="manage-comments.php">Approved Comments</a></li>
+                              </ul>
+                          </li>
 
-                  
 
-                </div>
-                <!-- Sidebar -left -->
+                      </ul>
+                  </div>
+                  <!-- Sidebar -->
+                  <div class="clearfix"></div>
 
-            </div>
+
+
+              </div>
+              <!-- Sidebar -left -->
+
+          </div>
