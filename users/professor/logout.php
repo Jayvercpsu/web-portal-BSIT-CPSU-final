d7884588d6bf5bc7ab076a93517495c20b894527
Jayvercpsu <jayver.cpsu@gmail.com>
2024-12-07 23:49:52 +0800

    fixed logout code profesor

--- users/professor/logout.php
+++ users/professor/logout.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
 include("includes/config.php");
-$_SESSION['login'] == "";
+
+// Clear all session variables
+$_SESSION = [];
+
+// Destroy the session
 session_unset();
 session_destroy();
 
-?>
-<script language="javascript">
-        document.location = "../login.php";
-</script>
+// Redirect to login page
+echo '<script language="javascript">document.location = "../login.php";</script>';
+exit; // Ensure no further script execution
