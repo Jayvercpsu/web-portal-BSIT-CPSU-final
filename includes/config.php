c7b6326fd136407dd48abefdb75dc925178827e6
joshxb2 <algadipej962@gmail.com>
2025-01-20 21:42:48 +0800

    update config

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', getenv('HEROKU_HOST') ? getenv('HEROKU_HOST') : 'localhost');
 define('DB_USER',  getenv('HEROKU_USERNAME') ? getenv('HEROKU_USERNAME') : 'root');
 define('DB_PASS', getenv('HEROKU_PASSWORD') ? getenv('HEROKU_PASSWORD') : '');
-define('DB_NAME', getenv('HEROKU_DB') ? getenv('HEROKU_DB') : 'vnhs');
+define('DB_NAME', getenv('HEROKU_DB') ? getenv('HEROKU_DB') : 'bsit_final');
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 // Check connection
 if (mysqli_connect_errno()) {
